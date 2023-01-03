ff690aa4626a9c575ccc1fe75664a11f205f0eb6
wndudwhdka <wndudwhdka@naver.com>
2023-01-03 18:20:16 +0900

    variable heritance

---
+++ problemSolving/basic/AbstractClass.java
@@ -0,0 +1,46 @@
+abstract class Pet{
+	abstract void walk();
+	abstract void balk();
+	// abstract int a; 변수는 불가.
+	protected void run()
+	{
+		System.out.println("run run");
+	}
+}
+
+class Dog extends Pet{
+	void walk() // 추상클래스에서 생성했던 abstract void walk를 정의 이작업을 오버라이딩이라고 한다. 
+	{
+		System.out.println("Dog walk..");
+	}
+	
+	void balk() // 미정의 시 에러.
+	{
+		System.out.println("waL! waL!");
+	}
+}
+
+class Cat extends Pet{
+	void walk() // 추상클래스에서 생성했던 abstract void walk를 정의 
+	{
+		System.out.println("Cat walk..");
+	}
+	
+	void balk() // 미정의 시 에러.
+	{
+		System.out.println("miyaonG~");
+	}
+}
+
+
+public class AbstractClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Dog dog = new Dog();
+		System.out.println(
+		
+	}
+
+}
---
+++ problemSolving/basic/Interface.java
@@ -0,0 +1,11 @@
+// 인터페이스, 다중 상속 
+// 추상클래스 보다 더 추상화된 개념으로 장점으로는 여러 클래스로 부터 하나의 클래스를 상속하여 준다는 점이 있다. 
+
+public class Interface {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
