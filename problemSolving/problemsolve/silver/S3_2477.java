1c5d18b124946f2c2de789868f0a70cba551e236
wndudwhdka <wndudwhdka@naver.com>
2023-02-15 18:17:58 +0900

    .

---
+++ problemSolving/problemsolve/silver/S3_2477.java
@@ -0,0 +1,15 @@
+package silver;
+
+import java.util.Scanner;
+
+public class S3_2477 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int k = sc.nextInt(); 
+		
+		int[] a = {0,0,0,0}; 
+		
+		
+		sc.close();
+	}
+}
